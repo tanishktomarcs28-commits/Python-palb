db1fcd75929210f86b59b8062789ba431bca4782
tanishktomarcs28-commits <tanishk.tomar.cs28@iilm.edu>
2026-04-25 09:23:53 +0530

    Create Jump Game II.py

---
+++ Jump Game II.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        jumps = 0
+        current_end = 0
+        farthest = 0
+
+        for i in range(len(nums) - 1):
+            farthest = max(farthest, i + nums[i])
+
+            # if we reach the end of current jump range
+            if i == current_end:
+                jumps += 1
+                current_end = farthest
+
+        return jumps
