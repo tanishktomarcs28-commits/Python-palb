a422674d77077cf70895d109eaf2a6aab5cc95c7
tanishktomarcs28-commits <tanishk.tomar.cs28@iilm.edu>
2026-04-25 09:15:37 +0530

    Create Reverse an Arra.py

---
+++ Reverse an Arra.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reverseArray(self, arr):
+        # code here
+        left = 0
+        right = len(arr) - 1
+        
+        while left < right:
+            arr[left], arr[right] = arr[right], arr[left]
+            left += 1
+            right -= 1
+        
+        return arr
+        
+        
