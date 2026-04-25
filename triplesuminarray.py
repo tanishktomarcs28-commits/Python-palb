890776192242815f41d40e19a32e38073086d89e
tanishktomarcs28-commits <tanishk.tomar.cs28@iilm.edu>
2026-04-25 09:06:22 +0530

    Add triplet sum checking method
    Implement a method to check for triplet sum in an array.

---
+++ triplesuminarray.py
@@ -0,0 +1,24 @@
+class Solution:
+    def hasTripletSum(self, arr, target):
+        # Code Here
+        
+    class Solution:
+    def hasTripletSum(self, arr, target):
+        arr.sort()
+        n = len(arr)
+        
+        for i in range(n - 2):
+            left = i + 1
+            right = n - 1
+            
+            while left < right:
+                curr_sum = arr[i] + arr[left] + arr[right]
+                
+                if curr_sum == target:
+                    return True
+                elif curr_sum < target:
+                    left += 1
+                else:
+                    right -= 1
+        
+        return False
