ae051cfc27afbb40985f9213d75ece55ee133f40
tanishktomarcs28-commits <tanishk.tomar.cs28@iilm.edu>
2026-04-25 09:25:15 +0530

    Create Group Anagrams.py

---
+++ Group Anagrams.py
@@ -0,0 +1,12 @@
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram_map = defaultdict(list)
+
+        for word in strs:
+            key = ''.join(sorted(word))  # sorted string as key
+            anagram_map[key].append(word)
+
+        return list(anagram_map.values())
