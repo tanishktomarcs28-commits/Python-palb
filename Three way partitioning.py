1c89dc6c36f786417ce1a897fa1ab86546c06ce1
tanishktomarcs28-commits <tanishk.tomar.cs28@iilm.edu>
2026-04-25 09:11:04 +0530

    Create Three way partitioning.py

---
+++ Three way partitioning.py
@@ -0,0 +1,25 @@
+#User function template for Python
+
+class Solution:
+    #Function to partition the array around the range such 
+    #that array is divided into three parts.
+	def threeWayPartition(self, arr, a, b):
+	    # code here 
+        n = len(arr)
+        
+        low = 0
+        mid = 0
+        high = n - 1
+        
+        while mid <= high:
+            if arr[mid] < a:
+                arr[low], arr[mid] = arr[mid], arr[low]
+                low += 1
+                mid += 1
+            elif a <= arr[mid] <= b:
+                mid += 1
+            else:  # arr[mid] > b
+                arr[mid], arr[high] = arr[high], arr[mid]
+                high -= 1
+        
+        return arr
